b8c7428a1dee3168a1d59d0b593a4319c921be40
Seph Gentle <me@josephg.com>
2022-03-11 21:03:42 +1100

    Moved testdata loading into a separate child package

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,68 +1,7 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput, BenchmarkId};
+use criterion::*;
+use crdt_testdata::*;
 
-// use std::time::SystemTime;
-use std::fs::File;
-use std::io::{BufReader, Read};
-use flate2::bufread::GzDecoder;
-use serde::Deserialize;
 use jumprope::JumpRope;
-
-/// This file contains some simple helpers for loading test data. Its used by benchmarking and
-/// testing code.
-
-/// (position, delete length, insert content).
-#[derive(Debug, Clone, Deserialize)]
-pub struct TestPatch(pub usize, pub usize, pub String);
-
-#[derive(Debug, Clone, Deserialize)]
-pub struct TestTxn {
-    // time: String, // ISO String. Unused.
-    pub patches: Vec<TestPatch>
-}
-
-#[derive(Debug, Clone, Deserialize)]
-pub struct TestData {
-    #[serde(rename = "startContent")]
-    pub start_content: String,
-    #[serde(rename = "endContent")]
-    pub end_content: String,
-
-    pub txns: Vec<TestTxn>,
-}
-
-impl TestData {
-    pub fn len(&self) -> usize {
-        self.txns.iter()
-            .map(|txn| { txn.patches.len() })
-            .sum::<usize>()
-    }
-
-    pub fn is_empty(&self) -> bool {
-        !self.txns.iter().any(|txn| !txn.patches.is_empty())
-    }
-}
-
-// TODO: Make a try_ version of this method, which returns an appropriate Error object.
-pub fn load_testing_data(filename: &str) -> TestData {
-    // let start = SystemTime::now();
-    // let mut file = File::open("benchmark_data/automerge-paper.json.gz").unwrap();
-    let file = File::open(filename).unwrap();
-
-    let reader = BufReader::new(file);
-    // We could pass the GzDecoder straight to serde, but it makes it way slower to parse for
-    // some reason.
-    let mut reader = GzDecoder::new(reader);
-    let mut raw_json = vec!();
-    reader.read_to_end(&mut raw_json).unwrap();
-
-    // println!("uncompress time {}", start.elapsed().unwrap().as_millis());
-
-    // let start = SystemTime::now();
-    let data: TestData = serde_json::from_reader(raw_json.as_slice()).unwrap();
-    // println!("JSON parse time {}", start.elapsed().unwrap().as_millis());
-
-    data
-}
 
 fn count_chars(s: &String) -> usize {
     s.chars().count()
